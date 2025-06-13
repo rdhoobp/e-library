531c65bc0e002ee0039cd5e40a5ecfc4123783eb
DimasIdha <dimasidhaw@gmail.com>
2025-06-13 07:10:09 +0700

    dbase change

--- elib.sql
+++ elib.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2025 at 10:10 AM
+-- Generation Time: Jun 13, 2025 at 02:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,71 @@ SET time_zone = "+00:00";
 --
 -- Database: `elib`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `book_id` varchar(20) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `publisher` varchar(100) NOT NULL,
+  `genre_id` int(11) NOT NULL,
+  `isbn` varchar(20) DEFAULT NULL,
+  `availability` tinyint(1) NOT NULL,
+  `role` int(11) NOT NULL,
+  `cover` varchar(250) NOT NULL,
+  `pdf_link` varchar(250) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_detail`
+--
+
+CREATE TABLE `book_detail` (
+  `book_detail_id` int(11) NOT NULL,
+  `book_id` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowing_history`
+--
+
+CREATE TABLE `borrowing_history` (
+  `borrow_id` int(11) NOT NULL,
+  `book_id` varchar(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `borrow_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `return_date` datetime DEFAULT NULL,
+  `fine` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `genre_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL,
+  `num_books` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -48,6 +113,31 @@ INSERT INTO `user` (`id_user`, `username`, `name`, `email`, `password`, `role`, 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`book_id`),
+  ADD UNIQUE KEY `isbn` (`isbn`);
+
+--
+-- Indexes for table `book_detail`
+--
+ALTER TABLE `book_detail`
+  ADD PRIMARY KEY (`book_detail_id`);
+
+--
+-- Indexes for table `borrowing_history`
+--
+ALTER TABLE `borrowing_history`
+  ADD PRIMARY KEY (`borrow_id`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`genre_id`);
 
 --
 -- Indexes for table `user`
