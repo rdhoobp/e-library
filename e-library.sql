f1cdedadc62d550fdd4ee41907678c682188b684
DimasIdha <dimasidhaw@gmail.com>
2025-06-27 01:22:04 +0700

    benerin database buku

--- e-library.sql
+++ e-library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2025 at 09:34 AM
+-- Generation Time: Jun 26, 2025 at 08:19 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -49,14 +49,14 @@ CREATE TABLE `book` (
 INSERT INTO `book` (`book_id`, `title`, `author`, `publisher`, `genre_id`, `isbn`, `availability`, `role`, `cover`, `pdf_link`, `created_at`, `updated_at`) VALUES
 ('001', 'Laut Bercerita', 'Leila S. Chudori', 'Kepustakaan Populer Gramedia', 1, '9786024246945', 1, 1, 'laut-bercerita-leila-s-chudori.jpg', 'none', '2025-06-23 02:09:28', NULL),
 ('002', 'Atomic Habits', 'James Clear', 'Kepustakaan Populer Gramedia', 1, '9780593189641', 1, 1, 'atomic-habits-james-clear.jpg', 'none', '2025-06-23 06:06:19', NULL),
-('003', 'The Psychology of Money', 'Morgan Housel', 'Kepustakaan Populer Gramedia', 1, '9786238371044', 1, 1, 'the-psychology-of-money-morgan-housel', 'none', '2025-06-23 06:16:01', NULL),
-('004', 'Sisi Tergelap Surga', 'Brian Khrisna', 'Kepustakaan Populer Gramedia', 1, '9786020674384', 1, 1, 'sisi-tergelap-surga-brian-khrisna', 'none', '2025-06-23 06:26:34', NULL),
-('005', 'Crypto Smart Money', 'Akademi Crypto', 'Kepustakaan Populer Gramedia', 1, '9786231909107', 1, 1, 'crypto-smart-money-akademi-crypto', 'none', '2025-06-23 06:49:33', NULL),
-('006', 'Teka Teki Rumah Aneh', 'Uketsu', 'Kepustakaan Populer Gramedia', 1, '9786020669960', 1, 1, 'teka-teki-rumah-aneh-uketsu', 'none', '2025-06-23 07:12:27', NULL),
-('007', 'Ruri Dragon 01', 'Masaoki Shindo', 'Kepustakaan Populer Gramedia', 1, '9786230316791', 1, 1, 'ruri-dragon-01-masaoki-shindo', 'none', '2025-06-23 07:16:07', NULL),
-('008', 'BEYOND AVERAGE', 'Kun Wahyu Wardana', 'Kepustakaan Populer Gramedia', 1, '9786020681580', 1, 1, 'beyond-average-kun-wahyu-wardana', 'none', '2025-06-23 07:25:32', NULL),
-('009', 'Dompet Ayah Sepatu Ibu', 'J.S Khairen', 'Kepustakaan Populer Gramedia', 1, '9786020530222', 1, 1, 'dompet-ayah-sepatu-ibu-j-s-khairen', 'none', '2025-06-23 07:28:30', NULL),
-('010', 'Naruto Bind Up Edition 17', 'MASASHI KISHIMOTO', 'Kepustakaan Populer Gramedia', 1, '9786230069765', 1, 1, 'naruto-bind-up-edition-17-masashi-kishimoto', 'none', '2025-06-23 07:33:06', NULL);
+('003', 'The Psychology of Money', 'Morgan Housel', 'Kepustakaan Populer Gramedia', 1, '9786238371044', 1, 1, 'the-psychology-of-money-morgan-housel.jpg', 'none', '2025-06-23 06:16:01', NULL),
+('004', 'Sisi Tergelap Surga', 'Brian Khrisna', 'Kepustakaan Populer Gramedia', 1, '9786020674384', 1, 1, 'sisi-tergelap-surga-brian-khrisna.jpg', 'none', '2025-06-23 06:26:34', NULL),
+('005', 'Crypto Smart Money', 'Akademi Crypto', 'Kepustakaan Populer Gramedia', 1, '9786231909107', 1, 1, 'crypto-smart-money-akademi-crypto.jpg', 'none', '2025-06-23 06:49:33', NULL),
+('006', 'Teka Teki Rumah Aneh', 'Uketsu', 'Kepustakaan Populer Gramedia', 1, '9786020669960', 1, 1, 'teka-teki-rumah-aneh-uketsu.jpg', 'none', '2025-06-23 07:12:27', NULL),
+('007', 'Ruri Dragon 01', 'Masaoki Shindo', 'Kepustakaan Populer Gramedia', 1, '9786230316791', 1, 1, 'ruri-dragon-01-masaoki-shindo.jpg', 'none', '2025-06-23 07:16:07', NULL),
+('008', 'BEYOND AVERAGE', 'Kun Wahyu Wardana', 'Kepustakaan Populer Gramedia', 1, '9786020681580', 1, 1, 'beyond-average-kun-wahyu-wardana.jpg', 'none', '2025-06-23 07:25:32', NULL),
+('009', 'Dompet Ayah Sepatu Ibu', 'J.S Khairen', 'Kepustakaan Populer Gramedia', 1, '9786020530222', 1, 1, 'dompet-ayah-sepatu-ibu-j-s-khairen.jpg', 'none', '2025-06-23 07:28:30', NULL),
+('010', 'Naruto Bind Up Edition 17', 'MASASHI KISHIMOTO', 'Kepustakaan Populer Gramedia', 1, '9786230069765', 1, 1, 'naruto-bind-up-edition-17-masashi-kishimoto.jpg', 'none', '2025-06-23 07:33:06', NULL);
 
 -- --------------------------------------------------------
 
@@ -140,7 +140,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `name`, `email`, `password`, `img`, `role`, `created_at`, `updated_at`) VALUES
-(5, 'admin', 'Raditya Abib', 'radityaabib511@gmail.com', '$2y$10$Fibb8YJmqGMEbQqXX0qhKOsJ6kmEBCPP5FUjcA.RSKu8wxpKslXRm', 'avatar1.jpg', 1, '2025-06-23 02:02:54', NULL);
+(5, 'admin', 'Raditya Abib', 'radityaabib511@gmail.com', '$2y$10$Fibb8YJmqGMEbQqXX0qhKOsJ6kmEBCPP5FUjcA.RSKu8wxpKslXRm', 'avatar1.jpg', 1, '2025-06-23 02:02:54', NULL),
+(7, 'rido', 'ridoanjai', 'anjairidoGanteng@gmail.com', '$2y$10$Up5Mt06d4rxoZGF40c1F9uJ/ea7xv7QOty7nOwOm9RKttNjxN6GtC', NULL, 0, '2025-06-25 06:14:33', NULL);
 
 --
 -- Indexes for dumped tables
@@ -185,7 +186,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
