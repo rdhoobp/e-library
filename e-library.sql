7b4a7dcba4cd833fb056b555d7bd37545678ef1b
DimasIdha <dimasidhaw@gmail.com>
2025-07-03 02:51:11 +0700

    pembaruan db mengapus table book_detail borrowing_history dan penambahan colom hit_counter di table book

--- e-library.sql
+++ e-library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2025 at 09:41 AM
+-- Generation Time: Jul 02, 2025 at 09:45 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -28,64 +28,33 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `book` (
-  `book_id` varchar(20) NOT NULL,
+  `book_id` int(11) NOT NULL,
   `title` varchar(200) NOT NULL,
   `author` varchar(100) NOT NULL,
   `publisher` varchar(100) NOT NULL,
   `genre_id` int(11) NOT NULL,
   `isbn` varchar(20) DEFAULT NULL,
-  `availability` tinyint(1) NOT NULL,
-  `role` int(11) NOT NULL,
   `cover` varchar(250) NOT NULL,
   `pdf_link` varchar(250) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT NULL
+  `updated_at` datetime DEFAULT NULL,
+  `hit_counter` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`book_id`, `title`, `author`, `publisher`, `genre_id`, `isbn`, `availability`, `role`, `cover`, `pdf_link`, `created_at`, `updated_at`) VALUES
-('001', 'Laut Bercerita', 'Leila S. Chudori', 'Kepustakaan Populer Gramedia', 1, '9786024246945', 1, 1, 'laut-bercerita-leila-s-chudori.jpg', 'none', '2025-06-23 02:09:28', NULL),
-('002', 'Atomic Habits', 'James Clear', 'Kepustakaan Populer Gramedia', 1, '9780593189641', 1, 1, 'atomic-habits-james-clear.jpg', 'none', '2025-06-23 06:06:19', NULL),
-('003', 'The Psychology of Money', 'Morgan Housel', 'Kepustakaan Populer Gramedia', 1, '9786238371044', 1, 1, 'the-psychology-of-money-morgan-housel.jpg', 'none', '2025-06-23 06:16:01', NULL),
-('004', 'Sisi Tergelap Surga', 'Brian Khrisna', 'Kepustakaan Populer Gramedia', 1, '9786020674384', 1, 1, 'sisi-tergelap-surga-brian-khrisna.jpg', 'none', '2025-06-23 06:26:34', NULL),
-('005', 'Crypto Smart Money', 'Akademi Crypto', 'Kepustakaan Populer Gramedia', 1, '9786231909107', 1, 1, 'crypto-smart-money-akademi-crypto.jpg', 'none', '2025-06-23 06:49:33', NULL),
-('006', 'Teka Teki Rumah Aneh', 'Uketsu', 'Kepustakaan Populer Gramedia', 1, '9786020669960', 1, 1, 'teka-teki-rumah-aneh-uketsu.jpg', 'none', '2025-06-23 07:12:27', NULL),
-('007', 'Ruri Dragon 01', 'Masaoki Shindo', 'Kepustakaan Populer Gramedia', 1, '9786230316791', 1, 1, 'ruri-dragon-01-masaoki-shindo.jpg', 'none', '2025-06-23 07:16:07', NULL),
-('008', 'BEYOND AVERAGE', 'Kun Wahyu Wardana', 'Kepustakaan Populer Gramedia', 1, '9786020681580', 1, 1, 'beyond-average-kun-wahyu-wardana.jpg', 'none', '2025-06-23 07:25:32', NULL),
-('009', 'Dompet Ayah Sepatu Ibu', 'J.S Khairen', 'Kepustakaan Populer Gramedia', 1, '9786020530222', 1, 1, 'dompet-ayah-sepatu-ibu-j-s-khairen.jpg', 'none', '2025-06-23 07:28:30', NULL),
-('010', 'Naruto Bind Up Edition 17', 'MASASHI KISHIMOTO', 'Kepustakaan Populer Gramedia', 1, '9786230069765', 1, 1, 'naruto-bind-up-edition-17-masashi-kishimoto.jpg', 'none', '2025-06-23 07:33:06', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `book_detail`
---
-
-CREATE TABLE `book_detail` (
-  `book_detail_id` int(11) NOT NULL,
-  `book_id` varchar(20) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `borrowing_history`
---
-
-CREATE TABLE `borrowing_history` (
-  `borrow_id` int(11) NOT NULL,
-  `book_id` varchar(20) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `borrow_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `return_date` datetime DEFAULT NULL,
-  `fine` int(11) NOT NULL,
-  `status` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `book` (`book_id`, `title`, `author`, `publisher`, `genre_id`, `isbn`, `cover`, `pdf_link`, `created_at`, `updated_at`, `hit_counter`) VALUES
+(1, 'Laut Bercerita', 'Leila S. Chudori', 'Kepustakaan Populer Gramedia', 1, '9786024246945', 'laut-bercerita-leila-s-chudori.jpg', 'none', '2025-06-23 02:09:28', NULL, 0),
+(2, 'Atomic Habits', 'James Clear', 'Kepustakaan Populer Gramedia', 1, '9780593189641', 'atomic-habits-james-clear.jpg', 'none', '2025-06-23 06:06:19', NULL, 0),
+(3, 'The Psychology of Money', 'Morgan Housel', 'Kepustakaan Populer Gramedia', 1, '9786238371044', 'the-psychology-of-money-morgan-housel.jpg', 'none', '2025-06-23 06:16:01', NULL, 0),
+(4, 'Sisi Tergelap Surga', 'Brian Khrisna', 'Kepustakaan Populer Gramedia', 1, '9786020674384', 'sisi-tergelap-surga-brian-khrisna.jpg', 'none', '2025-06-23 06:26:34', NULL, 0),
+(5, 'Teka Teki Rumah Aneh', 'Uketsu', 'Kepustakaan Populer Gramedia', 1, '9786020669960', 'teka-teki-rumah-aneh-uketsu.jpg', 'none', '2025-06-23 07:12:27', NULL, 0),
+(6, 'Ruri Dragon 01', 'Masaoki Shindo', 'Kepustakaan Populer Gramedia', 1, '9786230316791', 'ruri-dragon-01-masaoki-shindo.jpg', 'none', '2025-06-23 07:16:07', NULL, 0),
+(7, 'BEYOND AVERAGE', 'Kun Wahyu Wardana', 'Kepustakaan Populer Gramedia', 1, '9786020681580', 'beyond-average-kun-wahyu-wardana.jpg', 'none', '2025-06-23 07:25:32', NULL, 0),
+(8, 'Dompet Ayah Sepatu Ibu', 'J.S Khairen', 'Kepustakaan Populer Gramedia', 1, '9786020530222', 'dompet-ayah-sepatu-ibu-j-s-khairen.jpg', 'none', '2025-06-23 07:28:30', NULL, 0),
+(9, 'Naruto Bind Up Edition 17', 'MASASHI KISHIMOTO', 'Kepustakaan Populer Gramedia', 1, '9786230069765', 'naruto-bind-up-edition-17-masashi-kishimoto.jpg', 'none', '2025-06-23 07:33:06', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -155,18 +124,6 @@ ALTER TABLE `book`
   ADD UNIQUE KEY `isbn` (`isbn`);
 
 --
--- Indexes for table `book_detail`
---
-ALTER TABLE `book_detail`
-  ADD PRIMARY KEY (`book_detail_id`);
-
---
--- Indexes for table `borrowing_history`
---
-ALTER TABLE `borrowing_history`
-  ADD PRIMARY KEY (`borrow_id`);
-
---
 -- Indexes for table `genre`
 --
 ALTER TABLE `genre`
@@ -183,16 +140,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `book_detail`
+-- AUTO_INCREMENT for table `book`
 --
-ALTER TABLE `book_detail`
-  MODIFY `book_detail_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `borrowing_history`
---
-ALTER TABLE `borrowing_history`
-  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `book`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `genre`
